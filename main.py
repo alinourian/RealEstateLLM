99495a9faf0b5ded3da42fe981b0c4080ef26af8
Ali Nourian <64698354+alinourian10@users.noreply.github.com>
2023-10-03 20:26:48 +0330

    fix

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from controller import Menu
+from argparse import ArgumentParser
 from model.languageModels import ClassifierModel
 
 
-def run_program():
+def run_program(default_language="en"):
     # English,  Arabic,     Persian,    Urdu,   Chinese,        Russian,    Hindi
     # en        ar          fa          ur      zh-CN/zh-TW     ru          hi
-    default_language = "fa"
     menu = Menu(root_name='buy/rent', default_language=default_language)
     menu.start()
     print('=' * 50)
@@ -13,5 +13,12 @@ def run_program():
 
 
 if __name__ == '__main__':
-    run_program()
-
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--lang",
+        type=str,
+        required=False,
+        help="define the language (default=en)"
+    )
+    args = parser.parse_args()
+    run_program(args)
